0f0f40df25abb9812d7ded2afb08d63be4a2a837
muhafash <me.muhfarash@gmail.com>
2023-01-01 05:32:05 +0700

    Alhamdulillah

---
+++ app.js
@@ -0,0 +1,8 @@
+let js = prompt("Please Insert Sebuah Nilai");
+if (js === Number) {
+  for (let i = 0; i <= 10; i++) {
+    prompt("Please Reinsert a Valid Value Number.");
+  }
+} else {
+  alert("Thank you for being coperative.");
+}
